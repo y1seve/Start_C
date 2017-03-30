89b34fbd473cdbaba366136776e9607f6fdd5cf2
MartinYuS <yushuai09@gmail.com>
2017-03-30 23:44:42 +0800

    first commit, learn C

---
+++ GetStartedC/GetStartedC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  GetStartedC
+//
+//  Created by 余帅 on 17/3/30.
+//  Copyright © 2017年 余帅. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
