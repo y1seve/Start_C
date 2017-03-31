dde961dfbf3f88ed01a44f2dab1713c268795d8f
MartinYuS <yushuai09@gmail.com>
2017-03-31 22:08:46 +0800

    finish to chapter 11

--- GetStartedC/GetStartedC/main.c
+++ GetStartedC/GetStartedC/main.c
@@ -11,5 +11,14 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
+    int x = 1, y = 2, z[10];
+    int *ip;
+    ip = &x;
+    y = *ip;
+    *ip = 0;
+    ip = &z[0];
+    printf("x=%d y=%d\n", x, y);
     return 0;
 }
+
+
